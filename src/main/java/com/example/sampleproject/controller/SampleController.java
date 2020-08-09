0110c68605524da988139216612896c8e83c3a3c
seiya.tsukada <seiya.tsukada@mcb181-0127.lan>
2020-08-10 00:05:56 +0900

    add rest method

--- src/main/java/com/example/sampleproject/controller/SampleController.java
+++ src/main/java/com/example/sampleproject/controller/SampleController.java
@@ -1,13 +1,13 @@
 package com.example.sampleproject.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class SampleController {
 
-    @RequestMapping("/sample")
-    public String sample() {
+    @RequestMapping("/")
+    public String index() {
         return "sample";
     }
 }
